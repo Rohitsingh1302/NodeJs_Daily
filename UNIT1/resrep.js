2a93fee9fe1bddc4e5165971fab3205478d0fa24
rohit1302 <rohitnew1302@gmail.com>
2025-11-22 23:57:46 +0530

    done

--- UNIT1/resrep.js
+++ UNIT1/resrep.js
@@ -4,3 +4,11 @@ http.createServer(function(req,res){
     res.write("<h1>HAPPY DIWALI❤️</h1>");
     res.end();
 }).listen(3001);
+// Output:  
+// HAPPY DIWALI❤️
+// To test, run the server and navigate to http://localhost:3001 in your web browser.
+// The browser will display "HAPPY DIWALI❤️" as an H1 heading.
+// Note: Make sure to stop any other server running on port 3001 before testing this code.
+
+// This code creates a simple HTTP server that responds with "HAPPY DIWALI❤️" when accessed.
+
