fa5ce9591c5c5937d017bd2bc5b1893a262eed37
rohit1302 <rohitnew1302@gmail.com>
2025-11-23 23:22:09 +0530

    done

--- UNIT1/http.js
+++ UNIT1/http.js
@@ -3,3 +3,7 @@ http.createServer(function(req,res){
     res.write("<h1>Hello World</h1>");
     res.end();
 }).listen(3000);
+// Output:
+// When you run this code, it creates an HTTP server that listens on port 3000.
+// You can access it by navigating to http://localhost:3000 in your web browser,
+// and you will see "Hello World" displayed on the page.
