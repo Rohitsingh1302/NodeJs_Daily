8b2a4a3756334ff3bb63eaabcc4d21a4078b229b
rohit1302 <rohitnew1302@gmail.com>
2025-12-07 00:00:19 +0530

    done

--- UNIT1/Mongofirst.js
+++ UNIT1/Mongofirst.js
@@ -23,3 +23,4 @@ getData();
 //to insert data
 //db.videos.insertMany([{name:"video1",views:1000,likes:200},{name:"video2",views:2000,likes:500}])
 //db.videos.find().pretty() 
+//to exit mongo shell
