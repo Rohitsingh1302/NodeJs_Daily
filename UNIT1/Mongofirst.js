770104261f245297f5efb231c6035324813117e7
rohit1302 <rohitnew1302@gmail.com>
2025-12-09 11:33:54 +0530

    done

--- UNIT1/Mongofirst.js
+++ UNIT1/Mongofirst.js
@@ -11,17 +11,3 @@ async function getData(){
     console.log(data);
 }
 getData();
-//node UNIT1/Mongofirst.js
-//npm install mongodb
-//make sure mongodb server is running
-//mongod
-//mongo
-//show dbs
-//use youtube
-//show collections
-//db.videos.find().pretty()
-//to insert data
-//db.videos.insertMany([{name:"video1",views:1000,likes:200},{name:"video2",views:2000,likes:500}])
-//db.videos.find().pretty() 
-//to exit mongo shell
-//exit  
---
+++ UNIT1/Mongosecond.js
@@ -0,0 +1,32 @@
+const {MongoClient} = require('mongodb');
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+async function run(){
+    try{
+        await client.connect();
+        console.log("Connected successfully to server");
+        const db = client.db('LPU');
+        const users = db.collection('CLASS');
+
+        //CREATE 
+        await users.insertOne({name: "John Doe", age: 22, course: "Computer Science"});
+
+        //READ
+        const allUsers = await users.find({}).toArray();
+        console.log("All Users:", allUsers);
+
+        //UPDATE
+        await users.updateOne({name: "John Doe"}, {$set: {age: 23}});
+
+        //DELETE
+        await users.deleteOne({name: "John Doe"});
+
+        
+    }catch(err){
+        console.error(err);
+    }finally{
+        await client.close();
+    }
+}
+run();
