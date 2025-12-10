93d33c7b630409653ec3685015ca14de61016b36
rohit1302 <rohitnew1302@gmail.com>
2025-12-10 15:12:09 +0530

    done

--- UNIT1/mongoose.js
+++ UNIT1/mongoose.js
@@ -1,1 +1,18 @@
-const 
+const mongoose=require('mongoose');
+const main =async()=>{
+    try {
+        await mongoose.connect("mongodb://localhost:27017/school");
+        const StudentSchema=new mongoose.Schema({
+            name:String,
+            age:Number
+        });
+        const StudentModel=mongoose.model('Students',StudentSchema);
+        let data = new StudentModel({name:'John Doe',age:21});
+        let result=await data.save();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+main();
