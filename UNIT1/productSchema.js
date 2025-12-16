9414ece74ba222cc5d54992dc0b7bdc149946d75
rohit1302 <rohitnew1302@gmail.com>
2025-12-16 11:52:25 +0530

    done

--- UNIT1/productSchema.js
+++ UNIT1/productSchema.js
@@ -8,3 +8,22 @@ const productSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model('products', productSchema);
+require("./config");
+const express = require("express");
+const Product = require("./product");
+
+const app = express();
+app.use(express.json());
+
+app.post("/create", async (req, res) => {
+  const data = new Product(req.body);
+  const result = await data.save();
+  res.send(result);
+});
+
+app.get("/list", async (req, res) => {
+  const data = await Product.find();
+  res.send(data);
+});
+
+app.listen(5000);
